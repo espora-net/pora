949d8ee762ca07c8d3ed6deaeae5b8338b863bda
Carlos <carlos.hm@live.com>
2025-05-18 12:40:33 +0000

    v0.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import electron from 'vite-plugin-electron';
-import { resolve } from 'path';
+import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    electron([
-      {
-        entry: 'electron/main.ts',
-        vite: {
-          build: {
-            outDir: 'dist-electron',
-          },
-        },
-      },
-      {
-        entry: 'electron/preload.ts',
-        vite: {
-          build: {
-            outDir: 'dist-electron',
-          },
-        },
-        onstart: (options) => {
-          options.startup();
-        },
-      },
-    ]),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    port: 3000,
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "@/index.scss";',
+        api: "modern-compiler",
+      },
+    },
   },
-});
+})
